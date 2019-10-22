7cbe553db8f1e58d04cbeb4d8435272fc0331a14
Daniel Cripps <dan-cripps@hotmail.co.uk>
2019-10-22 13:05:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,17 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}"
+end
 
+def introduction_with_language_optional(name, language="ruby")
+  puts "Hi, my name is #{name} and i am learning to program in #{language}"
+end
 
 
 
